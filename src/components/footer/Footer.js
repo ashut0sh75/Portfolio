6a9371756e43a3c7d0dd5e333b8e213104d23f2f
Ashutosh Gupta <ashusanugupta999@gmail.com>
2023-11-21 19:34:45 +0530

    First Commit

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -14,7 +14,7 @@ export default function Footer() {
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
-          <a href="https://github.com/saadpasta/developerFolio">
+          <a href="https://github.com/ashut0sh75/Portfolio">
             Ashutosh Gupta
           </a>
         </p>
